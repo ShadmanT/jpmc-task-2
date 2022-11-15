21267c5a5faf53aa4efa86a6e0280b2edd24da3e
Shadman Tajwar <shadman.tajwar123@gmail.com>
2022-11-15 02:41:33 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
+import {setInterval} from 'timers';
 
 /**
  * State declaration for <App />
  */
 interface IState {
   data: ServerRespond[],
-  showGraph: boolean,
+  showGraph: boolean
 }
 
 /**
@@ -23,7 +24,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
-      showGraph: false, // makes sure that the graph is hidden when the app loads
+      showGraph: false // makes sure that the graph is hidden when the app loads
     };
   }
 
@@ -32,7 +33,7 @@ class App extends Component<{}, IState> {
    */
   renderGraph() {
     if (this.state.showGraph){ //renders the graph when showGraph is true
-      return (<Graph data={this.state.data}/>)
+        return (<Graph data={this.state.data}/>)
     }
   }
   /**
@@ -40,16 +41,19 @@ class App extends Component<{}, IState> {
    */
   getDataFromServer() {
     let x = 0;
-    const interval = setInterval(()=>{ // repeats the function 1000 times with a delay of 100 milliseconds
-      DataStreamer.getData((serverResponds: ServerRespond[]) => {
-        // Update the state by creating a new array of data that consists of
-        // Previous data in the state and the new data from server
-        this.setState({ data: [...this.state.data, ...serverResponds] });
-      });
-      x++;
-      if (x>1000){
-        clearInterval(interval);
-      }
+    // repeats the function 1000 times with a delay of 100 milliseconds
+    const interval = setInterval(() => { 
+
+        DataStreamer.getData((serverResponds: ServerRespond[]) => {
+          // Update the state by creating a new array of data that consists of
+          // Previous data in the state and the new data from server
+          this.setState({ data: [...this.state.data, ...serverResponds] });
+        });
+
+        x++;
+        if (x>1000){
+            clearInterval(interval);
+        }
     },100);
     
   }
@@ -60,23 +64,23 @@ class App extends Component<{}, IState> {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Bank & Merge Co Task 2
-        </header>
-        <div className="App-content">
-          <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
-            // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
-            Start Streaming Data
-          </button>
-          <div className="Graph">
-            {this.renderGraph()}
+          <header className="App-header">
+            Bank & Merge Co Task 2
+          </header>
+          <div className="App-content">
+              <button className="btn btn-primary Stream-button"
+                  // when button is click, our react app tries to request
+                  // new data from the server.
+                  // As part of your task, update the getDataFromServer() function
+                  // to keep requesting the data every 100ms until the app is closed
+                  // or the server does not return anymore data.
+                      onClick={() => {this.getDataFromServer()}}>
+                  Start Streaming Data
+              </button>
+              <div className="Graph">
+                {this.renderGraph()}
+              </div>
           </div>
-        </div>
       </div>
     )
   }
